d9e8e829ed5f2303e6c1c270a59f6add94093411
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-09-03 20:44:05 +0900

    script를 리액트에 추가하여 kakao로그인 구현 중 -> kakao.init을 못찾음

--- src/component/page/login.tsx
+++ src/component/page/login.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import KakaoLogin from '../service/kakaoLogin';
 const LoginPage = () : JSX.Element => {
-  return (<>로그인 페이지</>)
+  return (<>
+  <div>
+    <h2>로그인</h2>
+    <KakaoLogin />
+  </div>
+  </>)
 }
 export default LoginPage;
